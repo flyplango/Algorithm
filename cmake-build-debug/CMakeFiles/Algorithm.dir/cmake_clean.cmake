16a64b1976c918c421e558d8e7a36fbfe53fcfe0
jiaoml <919056489@qq.com>
2019-08-11 15:09:41 +0800

    GoogleKickStar: Round_D_2019_1

---
+++ cmake-build-debug/CMakeFiles/Algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Algorithm.dir/main.cpp.obj"
+  "CMakeFiles/Algorithm.dir/GoogleKickStar/Round_D_2019_1.cpp.obj"
+  "Algorithm.pdb"
+  "Algorithm.exe"
+  "Algorithm.exe.manifest"
+  "libAlgorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
